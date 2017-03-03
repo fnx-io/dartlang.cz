83cd243626324eaba537032a48329675610cb039
Tomucha <tomucha@gmail.com>
2017-03-03 09:46:53 +0100

    Ladeni textu.

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,11 @@
-// Copyright (c) 2017, tomucha. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'dart:html';
 
 void main() {
-  querySelector('#motto').innerHtml = '<p class="text--center">Pokud vidíte tento text: <span class="monospace fs-l">1!="1"</span>, pak váš prohlížeč podporuje Dart.</p>';
+  querySelector('#motto').innerHtml = '''
+    <p class="text--center">
+      Pokud vidíte tento text:
+      <span class="monospace fs-l">1!="1"</span>,<br>
+      pak váš prohlížeč podporuje Dart.
+    </p>
+  ''';
 }
