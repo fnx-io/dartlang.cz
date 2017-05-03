f7c56e6d2da78786dc9e0a04cd86f4bc1c55755d
Tomucha <tomucha@gmail.com>
2017-05-03 21:33:10 +0200

    Dalsi tip - Stream

--- web/dart/tips.dart
+++ web/dart/tips.dart
@@ -11,21 +11,34 @@ Future<Null> main() async {
   List<Tip> tips = [];
 
   const TOMUCHA = "Tomáš Zvěřina";
-  
+
+  tips.add(new Tip()
+    ..name="List (pole)"
+    ..description="List a pole - v Dartu jednou jsou."
+    ..dartPadId="e1f4e71b7d75d29cc7ea9e6870b502c4"
+    ..author = TOMUCHA
+  );
+
   tips.add(new Tip()
       ..name="Future"
-      ..description="Základy asynchronního programování pomocí Futures"
+      ..description="Základy asynchronního programování pomocí Futures."
       ..dartPadId="b887b707288c7be532237c466f63f273"
       ..author = TOMUCHA
   );
 
   tips.add(new Tip()
     ..name="async / await"
-    ..description="Pohodlné asynchronní programování pomocí 'await'"
+    ..description="Pohodlné asynchronní programování pomocí 'await'."
     ..dartPadId="3a611f5febde98d5de6c9fe56eb3431b"
     ..author = TOMUCHA
   );
 
+  tips.add(new Tip()
+    ..name="Stream"
+    ..description="Tok dat a událostí."
+    ..dartPadId="454a0e659a3aea422f78416ddd4ffef1"
+    ..author = TOMUCHA
+  );
   
   var benevolentValidator = new BenevolentNodeValidator();
   HtmlElement gallery = querySelector('#tips');
