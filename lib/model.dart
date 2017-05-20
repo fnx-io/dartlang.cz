980ab9e118560f202be4f239ff2c940b6040f66e
Honza Bittner <the@honzabittner.cz>
2017-05-20 12:06:31 +0200

    Unify quotes, equal marks, ...

--- lib/model.dart
+++ lib/model.dart
@@ -1,17 +1,14 @@
 class CaseStudy {
-
   String image;
   String name;
   String author;
   String link;
   String description;
-  
 }
-class Tip {
 
+class Tip {
   String name;
   String description;
   String dartPadId;
   String author;
-  
 }
--- web/dart/gallery.dart
+++ web/dart/gallery.dart
@@ -5,66 +5,65 @@ import 'package:dartlang/model.dart';
 import 'package:dartlang/templates.dart';
 
 Future<Null> main() async {
-
   await window.onLoad.first;
 
   List<CaseStudy> studies = [];
 
   studies.add(new CaseStudy()
-      ..author="Multimedia atelier s.r.o."
-      ..name="Spotřebitelská soutěž Flakes"
-      ..image="01.jpg"
-      ..link="http://www.stagexl.org/"
-      ..description='Webové "flashovky" pro iPady. Běžely v rámci spotřebitelské soutěže, která nebyla určená pro český trh. '
-          'Hry byly vytvořeny pomocí knihovny StageXL.'
+    ..author = "Multimedia atelier s.r.o."
+    ..name = "Spotřebitelská soutěž Flakes"
+    ..image = "01.jpg"
+    ..link = "http://www.stagexl.org/"
+    ..description = 'Webové "flashovky" pro iPady. Běžely v rámci spotřebitelské soutěže, která nebyla určená pro český trh. '
+        'Hry byly vytvořeny pomocí knihovny StageXL.'
   );
 
   studies.add(new CaseStudy()
-    ..author="Jana Moudrá"
-    ..name="Oficiální knihovna pro Firebase"
-    ..image="02.jpg"
-    ..link="https://pub.dartlang.org/packages/firebase"
-    ..description="Knihovna původně vznikla jako Open Source projekt, který později Google převzal a stal se oficiální implementací."
+    ..author = "Jana Moudrá"
+    ..name = "Oficiální knihovna pro Firebase"
+    ..image = '02.jpg'
+    ..link = 'https://pub.dartlang.org/packages/firebase'
+    ..description = "Knihovna původně vznikla jako Open Source projekt, který později Google převzal a stal se oficiální implementací."
   );
 
   studies.add(new CaseStudy()
-    ..author="fnx.io s.r.o."
-    ..name="fnx|ui"
-    ..image="03.jpg"
-    ..link="https://github.com/fnx-io/fnx_ui"
-    ..description="Knihovna Angular Dart komponent, která je určená pro rychlý vývoj informačních systémů, administračních rozhraní a CMS."
+    ..author = "fnx.io s.r.o."
+    ..name = "fnx|ui"
+    ..image = '03.jpg'
+    ..link = 'https://github.com/fnx-io/fnx_ui'
+    ..description = "Knihovna Angular Dart komponent, která je určená pro rychlý vývoj informačních systémů, administračních rozhraní a CMS."
   );
 
   studies.add(new CaseStudy()
-    ..author="Filip Hráček"
-    ..name="Tisíc knih"
-    ..image="04.jpg"
-    ..link="http://tisicknih.cz/"
-    ..description="Nástroj, který vám na základě dat Městské knihovny v Praze doporučí knihy, které by se Vám mohly líbit."
+    ..author = "Filip Hráček"
+    ..name = "Tisíc knih"
+    ..image = '04.jpg'
+    ..link = 'http://tisicknih.cz'
+    ..description = "Nástroj, který vám na základě dat Městské knihovny v Praze doporučí knihy, které by se Vám mohly líbit."
   );
 
   studies.add(new CaseStudy()
-    ..author="SnowlyCode s.r.o."
-    ..name="FxSimulátor"
-    ..image="06.jpg"
-    ..link="http://fxsimulator.com/"
-    ..description="FxSimulátor simuluje chování forexových trhů ve zrychleném čase. Zajišťuje bleskový a nenáročný vstup do problematiky."
+    ..author = "SnowlyCode s.r.o."
+    ..name = "FxSimulátor"
+    ..image = '06.jpg'
+    ..link = 'http://fxsimulator.com'
+    ..description = "FxSimulátor simuluje chování forexových trhů ve zrychleném čase. Zajišťuje bleskový a nenáročný vstup do problematiky."
   );
 
   studies.add(new CaseStudy()
-    ..author="Websitemaster a.s."
-    ..name="Dragito"
-    ..image="05.jpg"
-    ..link="https://www.dragito.com/"
-    ..description="Dragito je experimentální služba pro tvorbu webových stránek."
+    ..author = "Websitemaster a.s."
+    ..name = "Dragito"
+    ..image = '05.jpg'
+    ..link = 'https://www.dragito.com'
+    ..description = "Dragito je experimentální služba pro tvorbu webových stránek."
   );
 
   studies.add(new CaseStudy()
-    ..author="Státní fond životního prostředí"
-    ..name="Dotace Dešťovka"
-    ..image="07.jpg"
-    ..link="https://www.dotacedestovka.cz/"
-    ..description="Formuláře pro příjem žádostí v programu Dešťovka."
+    ..author = "Státní fond životního prostředí"
+    ..name = "Dotace Dešťovka"
+    ..image = '07.jpg'
+    ..link = 'https://www.dotacedestovka.cz'
+    ..description = "Formuláře pro příjem žádostí v programu Dešťovka."
   );
 
   studies.shuffle();
@@ -77,7 +76,7 @@ Future<Null> main() async {
   HtmlElement gallery = querySelector('#showCases');
 
   void closeLastDialog([_]) {
-    var dialog = querySelector("#caseStudyModal");
+    var dialog = querySelector('#caseStudyModal');
     if (dialog != null) dialog.remove();
   }
 
@@ -90,24 +89,21 @@ Future<Null> main() async {
       closeLastDialog();
       var dialog = new Element.html(renderDialog(o), validator: new BenevolentNodeValidator());
       document.body.append(dialog);
-      await dialog.querySelector(".modal__close").onClick.first;
+      await dialog.querySelector('.modal__close').onClick.first;
       dialog.remove();
     });
   }
-
-
 }
 
 class BenevolentNodeValidator implements NodeValidator {
 
   @override
   bool allowsAttribute(Element element, String attributeName, String value) {
-    return !attributeName.startsWith("on");
+    return !attributeName.startsWith('on');
   }
 
   @override
   bool allowsElement(Element element) {
-    return element.tagName.toLowerCase()!="script";
+    return element.tagName.toLowerCase() != 'script';
   }
-
 }
